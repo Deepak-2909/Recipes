382e803faff59a831380ef029ddf9d125dc3fc96
Deepak2909 <deepak19csu075@ncuindia.edu>
2021-06-03 00:53:20 +0530

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Deepak on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
